87c25cc9cd6348aaa97779dd6a3bdcf3008361ce
MohamedAshraf004 <lionelmido004@gmail.com>
2020-04-03 17:36:30 +0200

    Add SystemStatusPage viewComponent

---
+++ BethanysPieShop/Components/SystemStatusPage.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace BethanysPieShop.Components
+{
+    public class SystemStatusPage:ViewComponent
+    {
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var client = new HttpClient();
+            HttpResponseMessage responseMessage = await client.GetAsync("http://www.pluralsight.com");
+            if (responseMessage.StatusCode==System.Net.HttpStatusCode.OK)
+            {
+                return View(true);
+            }
+            return View(false);
+        }
+    }
+}
